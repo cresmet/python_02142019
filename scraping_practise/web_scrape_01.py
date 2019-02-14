8539d3757f56b1f27dcf2edac3a1a5b93ff6e535
Sudheer <Sudheer.Sanagala@specialized.com>
2019-02-14 11:18:28 -0800

    improved comments

--- scraping_practise/web_scrape_01.py
+++ scraping_practise/web_scrape_01.py
@@ -18,9 +18,8 @@ main_content = soup.find_all('div', attrs={'class': 'author-quotes'})
 # print(main_content.encode('utf-8'))
 
 for my_qt in main_content:
-    # print((my_qt.text).strip('\'"'))
-    # print re.sub('"', '', my_qt.text)
 
+    # using regex to stip double quotes
     s = my_qt.text
     s = re.sub(r'[^\w\s]', '', s)
     print(s)
