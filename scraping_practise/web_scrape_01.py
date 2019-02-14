c023212d42787e8e83e5bc480a5da159c48861ff
Sudheer <Sudheer.Sanagala@specialized.com>
2019-02-14 10:56:26 -0800

    first commit

--- scraping_practise/web_scrape_01.py
+++ scraping_practise/web_scrape_01.py
@@ -1,1 +1,26 @@
 import requests
+import lxml
+import re
+from bs4 import BeautifulSoup
+
+
+r = requests.get('https://www.keepinspiring.me/dr-seuss-quotes/')
+
+# print(r.content)
+
+c = r.content
+soup = BeautifulSoup(c, 'lxml')
+
+# print(soup.prettify().encode('utf-8'))
+
+main_content = soup.find_all('div', attrs={'class': 'author-quotes'})
+
+# print(main_content.encode('utf-8'))
+
+for my_qt in main_content:
+    # print((my_qt.text).strip('\'"'))
+    # print re.sub('"', '', my_qt.text)
+
+    s = my_qt.text
+    s = re.sub(r'[^\w\s]', '', s)
+    print(s)
